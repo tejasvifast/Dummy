e7b3087b8d4a14f945ba0fc66d443d97dc906129
pragya <pragyatiwari831@gmail.com>
2023-03-20 15:54:50 +0530

    fsdfs

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import './scss/style.scss'
 
-export const apiUrl= 'http://localhost:3000/'
+export const apiUrl= process.env.NODE_ENV==='development'? 'http://localhost:5000/':'https://warm-island-82496.herokuapp.com'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
